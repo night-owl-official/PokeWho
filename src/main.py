7d105d2669354aab6c64978e676748dcc52b5e2f
Daniel Grieco <daniel_grieco@programmer.net>
2023-04-15 21:25:59 +0200

    Import Pokemon cog and add it to the bot before running it asynchronously

--- src/main.py
+++ src/main.py
@@ -2,14 +2,19 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+import asyncio
+
+from pokemon_cog import PokemonCog
 
 load_dotenv()
 token = os.getenv("TOKEN")
 cmd_prefix = os.getenv("COMMAND_PREFIX")
 
-description = """Guess the Pokemon!
-The bot will show an image representing a random Pokemon,
-and the user will have to guess who that Pokemon is by entering its name."""
+description = """
+**Guess the Pokemon!**
+The bot will show an image representing a random Pokemon, 
+and the user will have to guess who that Pokemon is by entering its name.
+"""
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -23,4 +28,10 @@ async def on_ready():
     print(f"Logged in as {poke_who.user} (ID: {poke_who.user.id})")
     print("-------")
 
-poke_who.run(token)
+
+async def main():
+    async with poke_who:
+        await poke_who.add_cog(PokemonCog(poke_who))
+        await poke_who.start(token)
+
+asyncio.run(main())
