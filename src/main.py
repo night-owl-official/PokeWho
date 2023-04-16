bec24fc853648c3d64f3cbd218b815094ff947da
Daniel Grieco <daniel_grieco@programmer.net>
2023-04-16 21:53:29 +0200

    Implement command to sync command tree to the specified guilds or globally

--- src/main.py
+++ src/main.py
@@ -2,7 +2,9 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from discord.ext.commands import Greedy, Context
 import asyncio
+from typing import Literal, Optional
 
 from pokemon_cog import PokemonCog
 
@@ -26,6 +28,44 @@ poke_who = commands.Bot(command_prefix='?',
 async def on_ready():
     print(f"Logged in as {poke_who.user} (ID: {poke_who.user.id})")
     print("-------")
+
+
+@poke_who.command()
+@commands.guild_only()
+@commands.is_owner()
+async def sync(ctx: Context, guilds: Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    if not guilds:
+        if spec == "~":
+            # Sync commands to the current guild
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "*":
+            # Copy all global commands to the current guild and syncs
+            ctx.bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "^":
+            # Clear all commands from the current guild and syncs
+            ctx.bot.tree.clear_commands(guild=ctx.guild)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
+        else:
+            # Sync commands globally
+            synced = await ctx.bot.tree.sync()
+
+        await ctx.send(f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}")
+        return
+
+    # Sync commands to all given guilds and keep track of
+    # the successful attempts
+    ret = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+        except discord.HTTPException:
+            pass
+        else:
+            ret += 1
+
+    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
 
 
 async def main():
